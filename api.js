11638450b85805ba28ba60e3fd73ab8c2127ccd0
Armand <armand8@gmail.com>
2018-03-26 07:53:33 +0200

    mlab password

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 //====LIST DEPENDENCIES===//
-
 const express = require('express');
 const parseurl = require('parseurl');
 const bodyParser = require('body-parser');
@@ -8,8 +7,8 @@ const expressValidator = require('express-validator');
 const mongoose = require('mongoose');
 const Signature = require('./models/signature.js')
 const app = express();
-const url = "mongodb://armand8:react123@ds225308.mlab.com:25308/armand8";
-
+const url = "mongodb://armand8:sandrillon@ds225308.mlab.com:25308/armand8";
+//=========================//
 
 //=====SET BODY PARSER======//
 app.use(bodyParser.json());
@@ -21,6 +20,7 @@ app.get('/', function(req, res) {
   res.json('you did it');
 });
 //==========================//
+
 //====GET ALL SIGNATURES===//
 app.get('/api/signatures', function(req, res) {
   Signature.find({}).then(eachOne => {
@@ -28,6 +28,7 @@ app.get('/api/signatures', function(req, res) {
     })
   })
 //==========================//
+
 //====POST NEW SIGNATURE===//
 app.post('/api/signatures', function(req, res) {
        Signature.create({
